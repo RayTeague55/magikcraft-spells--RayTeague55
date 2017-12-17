3a04225b3b0024f1e4fd5bc058857f43cdcb0f3c
RayTeague55 <rayteague55@gmail.com>
2017-12-17 04:47:26 +0000

    commit spells

---
+++ write.ts
@@ -0,0 +1,11 @@
+const magik = magikcraft.io;
+
+function write() {
+    const font={
+    b:[192,192,192,192,252,,194,194,252]
+}
+function binary(letter){
+    return letter.map(line=>
+    line.tostring(2))
+}
+const=binary(font.a)
